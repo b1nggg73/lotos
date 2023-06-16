23c87d01f2edf8c620beedf98253217be461b8b4
b1nggg73 <93922501+b1nggg73@users.noreply.github.com>
2023-06-16 15:53:51 +0300

    Add files via upload

--- Lotus/js/background-image.js
+++ Lotus/js/background-image.js
@@ -3,7 +3,6 @@ const sliders = document.querySelectorAll('#sliders');
 
 for (let i = 0; i < sliders.length; i++) {
   const slider = sliders[i];
-
   slider.addEventListener('click', function () {
     wrapper.style.backgroundImage =
       "url('file:///C:/FrontEnd/Lotus/img/background-image" + i + ".jpg')";
@@ -14,3 +13,22 @@ for (let i = 0; i < sliders.length; i++) {
     slider.classList.remove('main__sliders-noneactive');
   });
 }
+
+let i = 0;
+
+const intervalId = setInterval(() => {
+  const slider = sliders[i];
+
+  wrapper.style.backgroundImage =
+    "url('file:///C:/FrontEnd/Lotus/img/background-image" + i + ".jpg')";
+  let sliderActive = document.querySelector('.main__sliders-active');
+  sliderActive.classList.remove('main__sliders-active');
+  sliderActive.classList.add('main__sliders-noneactive');
+  slider.classList.add('main__sliders-active');
+  slider.classList.remove('main__sliders-noneactive');
+  console.log(i);
+  i++;
+  if (i >= sliders.length) {
+    i = 0;
+  }
+}, 10000);
