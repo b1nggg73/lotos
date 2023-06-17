f93da071dfdd43f3d803f8d62bbe459bf7005b8e
b1nggg73 <93922501+b1nggg73@users.noreply.github.com>
2023-06-17 22:07:15 +0300

    Delete Lotus directory

--- Lotus/js/menu-burger.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector('.menu-btn');
-const nav = document.querySelector('.nav');
-
-menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('open');
-  nav.classList.toggle('nav__open');
-});
